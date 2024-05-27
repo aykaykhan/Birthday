bcecc04bd2ae5f79ab6a3358cd9d64e63137c41a
aykaykhan <abdullahhkhann169@gmail.com>
2024-05-27 21:56:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -246,7 +246,7 @@ const animationTimeline = () => {
         visibility: "visible",
         opacity: 0,
         scale: 80,
-        //repeat: 3,
+        repeat: 2,
         repeatDelay: 1.4
       },
       0.3
